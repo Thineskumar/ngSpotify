80204f27a94d6c96da4c55592b2bfcc0b8a37c02
Thineskumar <thineskumar@hotmail.se>
2017-01-16 15:40:06 +0100

    Data service added using spotify API

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {HttpModule} from '@angular/http';
 import { AppComponent }  from './app.component';
 import { SearchComponent} from './components/search/search.component';
 import { AboutComponent} from './components/about/about.component';
@@ -10,7 +10,7 @@ import { NavbarComponent} from './components/navbar/navbar.component';
 import { routing } from './app.routing';
 
 @NgModule({
-  imports:      [ BrowserModule, routing, FormsModule ],
+  imports:      [ BrowserModule, routing, FormsModule, HttpModule ],
   declarations: [ AppComponent, SearchComponent, AboutComponent, NavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
