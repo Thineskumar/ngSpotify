6bd385e2278ecb1a89c64828102ebf76053d5475
Thineskumar <thineskumar@hotmail.se>
2017-01-16 12:06:56 +0100

    git

---
+++ app/components/navabr/navbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'navbar',
+  templateUrl: 'navabr.component.html',
+})
+export class NavbarComponent  {  }
